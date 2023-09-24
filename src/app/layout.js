23b6275ce59bb77f006ecae59032dd5c87a58c1a
R <juandeossa.nc@gmail.com>
2023-09-23 21:21:10 -0500

    initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter,Roboto_Mono } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'],weight:['400','600','900'] })
+const roboto = Inter({ subsets: ['latin'],weight:['400','600','900'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   )
 }
